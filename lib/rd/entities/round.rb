bee1cfe952e7bf627acbc04363bb7b1c7c780f78
Brian Keane <lonesomewhistle@gmail.com>
2014-04-22 11:40:58 -0500

    round finished

--- lib/rd/entities/round.rb
+++ lib/rd/entities/round.rb
@@ -1,5 +1,14 @@
 module RD
-  class Round
+  class Round < Entity
+
+    attr_accessor :current_player_id, :current_scores, :starting_scores, :players, :round_over
+
+    def initialize(attrs)
+      super(attrs)
+      @current_player_id = 0
+      @round_over = false
+      @current_scores = starting_scores.dup
+    end
   end
 end
 
--- spec/entities/round_spec.rb
+++ spec/entities/round_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe 'Round' do
-  it "is initialized with player array, starting_score array, current_score array, current_player_id, round_over?" do
-    round = RD::Round.new({ players: ["Bob", "Bill", "Bubba"], starting_scores: [0,5,6], })
-    expect(round.players).to eq["Bob", "Bill", "Bubba"]
-    expect(starting_scores).to eq([0,5,6])
-    expect(current_player_id).to eq(0)
-    expect(round_over?).to eq(false)
-    expect(current_scores).to eq([0,5,6])
+  it "is initialized with player array, starting_score array, current_score array, current_player_id, round_over" do
+    round = RD::Round.new({ players: ["Bob", "Bill", "Bubba"], starting_scores: [0,5,6] })
+    expect(round.players).to eq(["Bob", "Bill", "Bubba"])
+    expect(round.starting_scores).to eq([0,5,6])
+    expect(round.current_player_id).to eq(0)
+    expect(round.round_over).to eq(false)
+    expect(round.current_scores).to eq([0,5,6])
   end
 
 end
