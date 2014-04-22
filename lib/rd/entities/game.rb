6214edddb3ac53831b01bc788ad2962bc26a7bd7
Ronak Patel <ronak@Ronaks-MacBook-Pro.local>
2014-04-22 11:37:03 -0500

    game tests pass

--- lib/rd/entities/game.rb
+++ lib/rd/entities/game.rb
@@ -1,10 +1,10 @@
 module RD
 	class Game
 		attr_accessor :players, :scores, :rounds
-		def initialize 
-			player = []
-			scores = []
-			rounds = []
+		def initialize(players)
+			@players = players
+			@scores = Array.new(@players.length){0}
+			@rounds = []
 		end 
 	end
 end
